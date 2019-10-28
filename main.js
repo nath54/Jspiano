846d53c54d9fa30a2296776410ba03caa0bb31f4
nath54 <cerisara.nathan@gmail.com>
2019-10-28 19:57:12 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,149 @@
+
+
+var notes=[];
+//                   notes 
+//           n°  MIDI     Latin    fréquence(Hz)
+notes.push(["01","A-1"  ,"La-2"     ,27.5]);
+notes.push(["02","A#-1" ,"La#-2"    ,29.1353]);
+notes.push(["03","B-1"  ,"Si-2"     ,30.8677]);
+notes.push(["04","C0"  ,"Do-1"     ,32.7032]);
+notes.push(["05","C#0"  ,"Do-1#"     ,34.6479]);
+notes.push(["06","D0"  ,"Re-1"     ,36.7081]);
+notes.push(["07","D#0"  ,"Re-1#"     ,38.8909]);
+notes.push(["08","E0"  ,"Mi-1"     ,41.2035]);
+notes.push(["09","F0"  ,"Fa-1"     ,43.6536]);
+notes.push(["10","F#0"  ,"Fa#-1"     ,46.2493]);
+notes.push(["11","G0"  ,"Sol-1"     ,48.9995]);
+notes.push(["12","G#0"  ,"Sol#-1"     ,51.9130]);
+notes.push(["13","A0"  ,"La-1"     ,55]);
+notes.push(["14","A#0"  ,"La#-1"     ,58.2705]);
+notes.push(["15","B0"  ,"Si-1"     ,61.7354]);
+notes.push(["16","C1"  ,"Do1"     ,65.4064]);
+notes.push(["17","C#1"  ,"Do#1"     ,69.2957]);
+notes.push(["18","D1"  ,"Re1"     ,73.4162]);
+notes.push(["19","D#1"  ,"Re#1"     ,77.7817]);
+notes.push(["20","E1"  ,"Mi1"     ,82.4069]);
+notes.push(["21","F1"  ,"Fa1"     ,87.3071]);
+notes.push(["22","F#1"  ,"Fa#1"     ,92.4986]);
+notes.push(["23","G1"  ,"Sol1"     ,97.9989]);
+notes.push(["24","G#1"  ,"Sol#1"     ,103.826]);
+notes.push(["25","A1"  ,"La1"     ,110]);
+notes.push(["26","A#1"  ,"La#1"     ,116.541]);
+notes.push(["27","B1"  ,"Si1"     ,123.471]);
+notes.push(["28","C2"  ,"Do2"     ,130.813]);
+notes.push(["29","C#2"  ,"Do#2"     ,138.591]);
+notes.push(["30","D2"  ,"Re2"     ,146.832]);
+notes.push(["31","D#2"  ,"Re#2"     ,155.563]);
+notes.push(["32","E2"  ,"Mi2"     ,164.814]);
+notes.push(["33","F2"  ,"Fa2"     ,174.614]);
+notes.push(["34","F#2"  ,"Fa#2"     ,184,997]);
+notes.push(["35","G2"  ,"Sol2"     ,195.998]);
+notes.push(["36","G#2"  ,"Sol#2"     ,207.652]);
+notes.push(["37","A2"  ,"La2"     ,220]);
+notes.push(["38","A#2"  ,"La#2"     ,233.082]);
+notes.push(["39","B2"  ,"Si2"     ,246.942]);
+notes.push(["40","C3"  ,"Do3"     ,261.626]);
+notes.push(["41","C#3"  ,"Do#3"     ,277.183]);
+notes.push(["42","D3"  ,"Re3"     ,293.665]);
+notes.push(["43","D#3"  ,"Re#3"     ,311.127]);
+notes.push(["44","E3"  ,"Mi3"     ,329.628]);
+notes.push(["45","F3"  ,"Fa3"     ,349.228]);
+notes.push(["46","F#3"  ,"Fa#3"     ,369.994]);
+notes.push(["47","G3"  ,"La-2"     ,391.995]);
+notes.push(["48","G#3"  ,"La-2"     ,415.305]);
+notes.push(["49","A3"  ,"La-2"     ,440]);
+notes.push(["50","A#3"  ,"La-2"     ,466.164]);
+notes.push(["51","B3"  ,"La-2"     ,493.883]);
+notes.push(["52","C4"  ,"La-2"     ,523.251]);
+notes.push(["53","C#4"  ,"La-2"     ,554.365]);
+notes.push(["54","D4"  ,"La-2"     ,587.33]);
+notes.push(["55","D#4"  ,"La-2"     ,622.254]);
+notes.push(["56","E4"  ,"La-2"     ,659.255]);
+notes.push(["57","F4"  ,"La-2"     ,698.456]);
+notes.push(["58","F#4"  ,"La-2"     ,739.989]);
+notes.push(["59","G4"  ,"La-2"     ,783.991]);
+notes.push(["60","G#4"  ,"La-2"     ,830.609]);
+notes.push(["61","A4"  ,"La-2"     ,880]);
+notes.push(["62","A#4"  ,"La-2"     ,932.328]);
+notes.push(["63","B4"  ,"La-2"     ,987.767]);
+notes.push(["64","C5"  ,"La-2"     ,1046.5]);
+notes.push(["65","C#5"  ,"La-2"     ,1108.73]);
+notes.push(["66","D5"  ,"La-2"     ,1174.66]);
+notes.push(["67","D#5"  ,"La-2"     ,1244.51]);
+notes.push(["68","E5"  ,"La-2"     ,1318.51]);
+notes.push(["69","F5"  ,"La-2"     ,1396.91]);
+notes.push(["70","F#5"  ,"La-2"     ,1479.98]);
+notes.push(["71","G5"  ,"La-2"     ,1567.98]);
+notes.push(["72","G#5"  ,"La-2"     ,1661.22]);
+notes.push(["73","A5"  ,"La-2"     ,1760]);
+notes.push(["74","A#5"  ,"La-2"     ,1864.66]);
+notes.push(["75","B5"  ,"La-2"     ,1975.53]);
+notes.push(["76","C6"  ,"La-2"     ,2093]);
+notes.push(["77","C#6"  ,"La-2"     ,2217.46]);
+notes.push(["78","D6"  ,"La-2"     ,2349.32]);
+notes.push(["79","D#6"  ,"La-2"     ,2489.02]);
+notes.push(["80","E6"  ,"La-2"     ,2637.02]);
+notes.push(["81","F6"  ,"La-2"     ,2793.83]);
+notes.push(["82","F#6"  ,"La-2"     ,2959.96]);
+notes.push(["83","G6"  ,"La-2"     ,3135.96]);
+notes.push(["84","G#6"  ,"La-2"     ,3322.44]);
+notes.push(["85","A6"  ,"La-2"     ,3520]);
+notes.push(["86","A#6"  ,"La-2"     ,3729.31]);
+notes.push(["87","B6"  ,"La-2"     ,3951.07]);
+notes.push(["88","C7"  ,"La-2"     ,4186.01]);
+
+for(x=1;x<88;x++){
+    var b=document.getElementById(""+x);
+    var h=document.createElement("h4");
+    h.innerHTML=notes[x-1][2];
+    var p=document.createElement("p");
+    p.innerHTML=notes[x-1][4];
+    b.appendChild(h);
+    b.appendChild(p);
+}
+
+
+// crée un contexteaudio
+var contexteAudio = new (window.AudioContext || window.webkitAudioContext)();
+
+
+function suiteNote(oscillator){
+    oscillator.stop();
+}
+
+function note(num){
+    di=num-1
+    n=notes[di]
+    var oscillator = contexteAudio.createOscillator();
+    oscillator.connect(contexteAudio.destination);
+    oscillator.type = 'square';
+    oscillator.frequency.value = n[3]; // valeur en hertz
+    oscillator.start();
+    setTimeout(suiteNote, 50, args=(oscillator)); //Attendez 1 secondes avant de continuer dans la fonction suivante
+}
+
+var isshift=false;
+
+document.addEventListener('keydown', (event) => {
+    const nomTouche = event.key;
+    if(nomTouche=="Shift"){
+    isshift=true;
+    }
+    else{
+        if(isshift){
+            nomTouche.toUpperCase();
+            isshift=false;
+        }
+        for(n of notes){
+            if(n[4]==nomTouche){
+                note(notes.indexOf(n));
+            }
+        }
+    }
+},false);
+
+
+
+
+
+
